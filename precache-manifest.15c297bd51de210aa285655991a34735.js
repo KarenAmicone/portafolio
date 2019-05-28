f4ab3d807914f77fd8c0a774861a0dc8ed12228f
KarenAmicone <karen_amicone@hotmail.com>
2019-05-28 10:54:07 -0500

    Updates

---
+++ precache-manifest.15c297bd51de210aa285655991a34735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d853d6baaa80970e100ab85bfe91dae2",
+    "url": "/KarenAmicone/portafolio/index.html"
+  },
+  {
+    "revision": "f2ea265e1faceaf885e7",
+    "url": "/KarenAmicone/portafolio/static/css/main.4fbbc6ea.chunk.css"
+  },
+  {
+    "revision": "7d824d3efaf343d626f9",
+    "url": "/KarenAmicone/portafolio/static/js/2.ae742f72.chunk.js"
+  },
+  {
+    "revision": "f2ea265e1faceaf885e7",
+    "url": "/KarenAmicone/portafolio/static/js/main.ed54868c.chunk.js"
+  },
+  {
+    "revision": "e7976f352786f5046d6c",
+    "url": "/KarenAmicone/portafolio/static/js/runtime~main.60633239.js"
+  },
+  {
+    "revision": "bbe6deddbc4ec2b7714bd8e6dca56999",
+    "url": "/KarenAmicone/portafolio/static/media/Amicone-06.bbe6dedd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/KarenAmicone/portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
