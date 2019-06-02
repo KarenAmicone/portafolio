91d9176879ac200e3513bb877f946ce8c310f7d2
KarenAmicone <karen_amicone@hotmail.com>
2019-06-02 08:04:14 -0500

    subiendo burger

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,44 +2,31 @@ import React from 'react';
 import github from '../assets/github.png'
 import instagram from '../assets/instagram.png'
 import linkedin from '../assets/linkedin.png'
-import git from '../assets/git.png'
-import css from '../assets/css5.png'
-import node from '../assets/nodejs.png'
-import html from '../assets/html5.png'
-import js from '../assets/js.png'
-import npm from '../assets/npm.png'
-
-const projects = [
-    {
-        title: "your-md-links",
-        img: "https://github.com/KarenAmicone/portafolio/blob/master/src/assets/mdlinks.png?raw=true",
-        description: "Is a npm package that reads Markdown files to identify its links, count them and verify their response status." ,
-        repo: "https://github.com/KarenAmicone/CDMX007-fe-md-links",
-        demo: "https://www.npmjs.com/package/your-md-links",
-        tools: [{node}, {git}, {github}, {npm}]
-    }
-]
+import projects from '../projects.json'
 
 const Work = () => {
     return(
         <article className="work">
             {projects.map(project =>(
-                <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src={project.img}></img>
-                </div>
-                <div class="card-content">
-                <span class="card-title activator grey-text text-darken-4">{project.title}<i class="material-icons right">more_vert</i></span>
-                <p><a href={project.repo}>Repository</a></p>
-                <p><a href={project.demo}>Demo</a></p>
-                </div>
-                <div class="card-reveal">
-                <span class="card-title grey-text text-darken-4">{project.title}<i class="material-icons right">close</i></span>
-                <p>{project.description}</p>
-                {project.tools.map(tool=>(
-                    <img src={tool.dir} alt={tool.name}></img>
-                ))}
-                </div>
+                <div className="card" key={project.title}>
+                    <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src={project.img}></img>
+                    </div>
+                    <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">{project.title}<i className="material-icons right">more_vert</i></span>
+                        <p><a href={project.repo}>Repository</a></p>
+                        <p><a href={project.demo}>Demo</a></p>
+                    </div>
+                    <div className="card-reveal">
+                        <span className="card-title grey-text text-darken-4">{project.title}<i className="material-icons right">close</i></span>
+                        <p>{project.description}</p>
+                        <p className="card-title grey-text text-darken-4">Tools</p>
+                        <article className="tools-container">
+                        {project.tools.map(tool=>(
+                            <img src={tool.href} alt={tool.name} key={tool.name} className="tool"></img>
+                        ))}
+                        </article>
+                    </div>
             </div>
             ))}
 
