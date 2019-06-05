e3434701883c583d79dcd1000845bd0b2ca6beb2
KarenAmicone <karen_amicone@hotmail.com>
2019-06-04 21:22:55 -0500

    publicando img

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 <br></br>
                     <p>Since last January I've been working at Laboratoria, where I've created several apps with Javascript vanilla and React.</p> 
                     <br></br>
-                    <p>Currently, I'm developing a native app with React Native.</p>
+                    <p>I'm currently developing a native app with React Native.</p>
                     <br></br>
                     <p>In addition to web development, I studied a bachelor's degree in Economics at UNAM, which has made it easier for me to start this path due to my self-taught skills, work organization and logical thinking.</p> 
                     <br></br>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import projects from '../projects.json'
+import github from '../assets/github.png'
 
 const Work = () => {
     return(
@@ -8,7 +9,8 @@ const Work = () => {
             <h2>Recent Work</h2>
             <br></br>
             <p>The following are the most important projects in which I've been working in.</p>
-            <a href="https://github.com/KarenAmicone">If you want to see all my work, click here</a>
+            <p>If you want to see all my work, click here: </p>
+            <a href="https://github.com/KarenAmicone"><img src={github} alt="github" id="social-media"></img> Github</a>
         </article>
         <article className="work-container">
             {projects.map(project=>(
